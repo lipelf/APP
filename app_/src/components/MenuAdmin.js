770f3bc50b18e4234a1ddc272b047edf6a119fc9
lipelf <luizfrs2004@gmail.com>
2024-11-13 00:33:13 -0300

    criação do appointments

--- app_/src/components/MenuAdmin.js
+++ app_/src/components/MenuAdmin.js
@@ -6,6 +6,7 @@ export default function MenuAdmin() {
     <div className="d-flex justify-content-start">
         <div className="p-2"><Link className="navbar-brand" href="#">Admin</Link></div>
         <div className="p-2"><Link className="nav-link" href="/admin/users">Usuários</Link></div>
+        <div className="p-2"><Link className="nav-link" href="/admin/appointments">Appointments</Link></div>x
       </div>
 
     )
